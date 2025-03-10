e3308448a65812d788c2b0dce2a1140ccf275902
BryanGallo <bryangallo95@hotmail.com>
2025-03-09 23:42:04 -0500

    add: Routing con Link entre Login y Registro

--- app/auth/register/page.tsx
+++ app/auth/register/page.tsx
@@ -1,5 +1,6 @@
 import RegisterForm from "@/components/auth/RegisterForm";
 import { Metadata } from "next";
+import Link from "next/link";
 
 export const metadata: Metadata = {
     title: "PV - Register",
@@ -16,6 +17,11 @@ export default function RegisterPage() {
                 y vende tus <span className="text-amber-500">productos</span>
             </p>
             <RegisterForm />
+            <nav>
+                <Link href={"/auth/login"}>
+                    Ya tienes cuenta ? Iniciar Sesión
+                </Link>
+            </nav>
         </>
     );
 }
