b9e78037e22385fcd67f39c291426bd658b0686a
BryanGallo <bryangallo95@hotmail.com>
2025-03-19 13:59:34 -0500

    fix:Correccion Logo

--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -9,10 +9,14 @@ export default function AuthLayout({
         <>
             <div className="lg:grid lg:grid-cols-2 lg:min-h-screen">
                 <div className="bg-purple-950 min-h-32 lg:bg-auth lg:bg-30 bg-no-repeat bg-left-bottom">
-                    <Logo />
+                    <div className="w-96 py-10 lg:py-20 mx-auto">
+                        <Logo />
+                    </div>
                 </div>
                 <div className="p-3 lg:py-10 flex flex-col justify-center items-center">
-                    <div className="max-w-xl w-full flex flex-col">{children}</div>
+                    <div className="max-w-xl w-full flex flex-col">
+                        {children}
+                    </div>
                 </div>
             </div>
         </>
--- components/ui/Logo.tsx
+++ components/ui/Logo.tsx
@@ -1,6 +1,14 @@
 import Image from "next/image";
 
 export default function Logo() {
-    //TODO: Cambiar por la etique Img de Next
-    return <Image src="/logo.svg" alt="Logo Local" width={0} height={0} className="w-400 h-100" priority />;
+    return (
+        <Image
+            src="/logo.svg"
+            alt="Logo Local"
+            width={0}
+            height={0}
+            className="max-w-96 w-96"
+            priority
+        />
+    );
 }
