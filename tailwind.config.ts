9b1429370674ef738571cd3328aa1c703aa2465f
BryanGallo <bryangallo95@hotmail.com>
2025-03-20 13:09:40 -0500

    feat: Instalando shadcn

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,37 +1,81 @@
 import type { Config } from "tailwindcss";
 
 export default {
-  content: [
+    darkMode: ["class"],
+    content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-        'aquamarine': {
-          '50': '#e9fff7',
-          '100': '#c9ffeb',
-          '200': '#98ffdd',
-          '300': '#57fecf',
-          '400': '#30f3c2',
-          '500': '#00d9a5',
-          '600': '#00b288',
-          '700': '#008e71',
-          '800': '#00705a',
-          '900': '#005c4c',
-          '950': '#00342c',
-        },
-      },
-      backgroundImage:{
-        "auth":"url('/grafico.svg')"
-      },
-      backgroundSize:{
-        '30':'30rem'
-      }
-    },
+  	extend: {
+  		colors: {
+  			background: 'hsl(var(--background))',
+  			foreground: 'hsl(var(--foreground))',
+  			aquamarine: {
+  				'50': '#e9fff7',
+  				'100': '#c9ffeb',
+  				'200': '#98ffdd',
+  				'300': '#57fecf',
+  				'400': '#30f3c2',
+  				'500': '#00d9a5',
+  				'600': '#00b288',
+  				'700': '#008e71',
+  				'800': '#00705a',
+  				'900': '#005c4c',
+  				'950': '#00342c'
+  			},
+  			card: {
+  				DEFAULT: 'hsl(var(--card))',
+  				foreground: 'hsl(var(--card-foreground))'
+  			},
+  			popover: {
+  				DEFAULT: 'hsl(var(--popover))',
+  				foreground: 'hsl(var(--popover-foreground))'
+  			},
+  			primary: {
+  				DEFAULT: 'hsl(var(--primary))',
+  				foreground: 'hsl(var(--primary-foreground))'
+  			},
+  			secondary: {
+  				DEFAULT: 'hsl(var(--secondary))',
+  				foreground: 'hsl(var(--secondary-foreground))'
+  			},
+  			muted: {
+  				DEFAULT: 'hsl(var(--muted))',
+  				foreground: 'hsl(var(--muted-foreground))'
+  			},
+  			accent: {
+  				DEFAULT: 'hsl(var(--accent))',
+  				foreground: 'hsl(var(--accent-foreground))'
+  			},
+  			destructive: {
+  				DEFAULT: 'hsl(var(--destructive))',
+  				foreground: 'hsl(var(--destructive-foreground))'
+  			},
+  			border: 'hsl(var(--border))',
+  			input: 'hsl(var(--input))',
+  			ring: 'hsl(var(--ring))',
+  			chart: {
+  				'1': 'hsl(var(--chart-1))',
+  				'2': 'hsl(var(--chart-2))',
+  				'3': 'hsl(var(--chart-3))',
+  				'4': 'hsl(var(--chart-4))',
+  				'5': 'hsl(var(--chart-5))'
+  			}
+  		},
+  		backgroundImage: {
+  			'auth': "url('/grafico.svg')"
+  		},
+  		backgroundSize: {
+  			'30': '30rem'
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	}
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 } satisfies Config;
