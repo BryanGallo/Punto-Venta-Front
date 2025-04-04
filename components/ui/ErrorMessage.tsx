a533ae92914d494c9ff194882482eac264d4e279
BryanGallo <bryangallo95@hotmail.com>
2025-04-04 00:26:38 -0500

    refactor: Estilos alertas

--- components/ui/ErrorMessage.tsx
+++ components/ui/ErrorMessage.tsx
@@ -1,6 +1,6 @@
 export default function ErrorMessage({ error }: { error: string }) {
     return (
-        <p className="bg-red-500 rounded-lg text-white p-2 text-center uppercase">
+        <p className="bg-red-100 border border-red-400 rounded-lg text-red-700 p-2 text-center uppercase">
             {error}
         </p>
     );
