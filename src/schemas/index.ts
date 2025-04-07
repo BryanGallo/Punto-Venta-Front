b9aa7567ffeba26ce87fec5959df53bf05ee4aea
BryanGallo <bryangallo95@hotmail.com>
2025-04-06 23:09:08 -0500

    feat: Schemas para Success y error Crear Usuarios

--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -5,18 +5,24 @@ export const RegisterSchema = z
         email: z
             .string()
             .min(1, { message: "El email es obligatorio" })
-            .email({message:"Email no valido"}),
+            .email({ message: "Email no valido" }),
         name: z.string().min(1, { message: "El nombre es obligatorio" }),
         password: z.string().min(6, {
             message: "La contraseña debe ser de mínimo de 6 caracteres",
         }),
-        password_confirmation: z
-            .string()
-            .min(6, {
-                message: "Las contraseñas no son iguales",
-            }),
+        password_confirmation: z.string().min(6, {
+            message: "Las contraseñas no son iguales",
+        }),
     })
-    .refine((data) => data.password === data.password_confirmation,{
-        message:'Las contraseñas deben ser iguales',
-        path:['password_confirmation'] //*Indicamos donde debe colocarse el error
+    .refine((data) => data.password === data.password_confirmation, {
+        message: "Las contraseñas deben ser iguales",
+        path: ["password_confirmation"], //*Indicamos donde debe colocarse el error
     });
+
+export const SuccessSchema = z.object({
+    message: z.string(),
+});
+
+export const ErrorSchema = z.object({
+    message: z.string().array(),
+});
