ba33cf0049bed59727b40c4917e8bd4da6173dd2
BryanGallo <bryangallo95@hotmail.com>
2025-04-21 19:29:00 -0500

    feat: pantallas para token y restablecer contraseña

---
+++ components/auth/PasswordResetHandler.tsx
@@ -0,0 +1,12 @@
+"use client";
+import { useState } from "react";
+import ResetPasswordForm from "./ResetPasswordForm";
+import ValidateTokenForm from "./ValidateTokenForm";
+export default function PasswordResetHandler() {
+    const [isValidToken, setIsValidToken] = useState(false);
+  return (
+        <>
+            {!isValidToken ? <ValidateTokenForm /> : <ResetPasswordForm />}
+        </>
+    );
+}
