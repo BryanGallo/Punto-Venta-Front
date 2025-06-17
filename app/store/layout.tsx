393bd8916d75be0b1265aaaf83f8af405989adce
BryanGallo <bryangallo95@hotmail.com>
2025-06-17 14:29:21 -0500

    add: Agregando layout para Store

---
+++ app/store/layout.tsx
@@ -0,0 +1,15 @@
+export default function StoreLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <>
+            <main className="lg:flex  lg:h-screen lg:overflow-y-hidden">
+                <div className="md:flex-1 md:h-screen md:overflow-y-scroll pt-10  pb-32 px-10">
+                    {children}
+                </div>
+            </main>
+        </>
+    );
+}
