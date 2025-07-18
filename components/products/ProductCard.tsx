9fb1acf5f39e05bab96fb840f4b7a25ba0775cf5
BryanGallo <bryangallo95@hotmail.com>
2025-07-18 10:00:24 -0500

    add: agregando imagen usando Image de next

--- components/products/ProductCard.tsx
+++ components/products/ProductCard.tsx
@@ -1,12 +1,18 @@
-"use client";
-
 import { Product } from "@/src/schemas";
 import { formatCurrency } from "@/src/utils";
+import Image from "next/image";
 
 export default function ProductCard({ product }: { product: Product }) {
     return (
         <div className="rounded bg-white shadow relative p-5">
             <div>
+                <Image
+                    src={`${process.env.URL_IMAGE}/img/${product.image}`}
+                    alt={product.name}
+                    width={400}
+                    height={600}
+                    className="w-full h-52 object-cover"
+                />
                 <div className="p-3 space-y-2">
                     <h3 className="text-xl font-bold text-gray-600">{product.name}</h3>
                     <p className="text-gray-500">Disponibles:{product.inventory}</p>
