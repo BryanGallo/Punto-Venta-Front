228c1e7d38ff42512497910f4dfd6ca2c6bf2cc9
BryanGallo <bryangallo95@hotmail.com>
2025-07-19 11:40:48 -0500

    add: Componente ShoppingCart

---
+++ components/cart/ShoppingCart.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function ShoppingCart() {
+    return (
+        <>
+            <h2 className="text-2xl font-bold">Resumen de la compra</h2>
+        </>
+    );
+}
