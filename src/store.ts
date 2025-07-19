2ded07916c7539e46dca0e6f030f05201a20cb09
BryanGallo <bryangallo95@hotmail.com>
2025-07-19 12:10:49 -0500

    feat: contents en store

--- src/store.ts
+++ src/store.ts
@@ -1,13 +1,15 @@
 import { create } from "zustand";
-import { Product } from "./schemas";
+import { Product, ShoppingCart } from "./schemas";
 
 interface Store {
     total: number;
+    contents: ShoppingCart;
     addToCart: (product: Product) => void;
 }
 
 export const useStore = create<Store>(() => ({
     total: 0,
+    contents: [],
     addToCart: (product: Product) => {
         console.log("addToCart", product);
     },
