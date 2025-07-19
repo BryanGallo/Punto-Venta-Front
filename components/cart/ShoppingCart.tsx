049fc9406ba8c6821b0ad62a86773072a0066376
BryanGallo <bryangallo95@hotmail.com>
2025-07-19 14:20:27 -0500

    feat: CardItem como lista en ShoppingCart

--- components/cart/ShoppingCart.tsx
+++ components/cart/ShoppingCart.tsx
@@ -1,9 +1,22 @@
-import React from "react";
+"use client";
+
+import { useStore } from "@/src/store";
+import ShoppingCartItem from "./ShoopingCartItem";
 
 export default function ShoppingCart() {
+    const contents = useStore((state) => state.contents);
+    // console.log(contents);
     return (
         <>
             <h2 className="text-2xl font-bold">Resumen de la compra</h2>
+            <ul
+                role="list"
+                className="mt-6 divide-y divide-gray-200 border-t border-gray-200 text-sm text-gray-500"
+            >
+                {contents.map((item) => (
+                    <ShoppingCartItem key={item.productId} item={item} />
+                ))}
+            </ul>
         </>
     );
 }
