7f47f079f4414f2b9cc505654af986d5aeacb78e
BryanGallo <bryangallo95@hotmail.com>
2025-07-19 10:27:54 -0500

    add: Agregando MainNav al Layout

--- app/store/layout.tsx
+++ app/store/layout.tsx
@@ -1,3 +1,5 @@
+import MainNav from "@/components/ui/MainNav";
+
 export default function StoreLayout({
     children,
 }: Readonly<{
@@ -5,10 +7,12 @@ export default function StoreLayout({
 }>) {
     return (
         <>
-            <main className="lg:flex  lg:h-screen lg:overflow-y-hidden">
+            <MainNav />
+            <main className="lg:flex lg:h-screen lg:overflow-y-hidden">
                 <div className="md:flex-1 md:h-screen md:overflow-y-scroll pt-10  pb-32 px-10">
                     {children}
                 </div>
+                <aside className="md:w-96 md:h-screen md:overflow-y-scroll pt-10 pb-32 px-5 "></aside>
             </main>
         </>
     );
