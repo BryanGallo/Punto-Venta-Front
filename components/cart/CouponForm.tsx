a1750309a2bb4fbc340f6d1bb502c5185cbbb5c7
BryanGallo <bryangallo95@hotmail.com>
2025-08-20 23:58:27 -0500

    feat: creando formulario para cupon

--- components/cart/CouponForm.tsx
+++ components/cart/CouponForm.tsx
@@ -1,8 +1,22 @@
+import { FormEvent } from "react";
+
 export default function CouponForm() {
-  return (
-        <form className="flex flex-col gap-2">
-            <input type="text" placeholder="Código de descuento" />
+    const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        const formData = new FormData(e.currentTarget);
+        console.log(formData.get("coupon"));
+        const coupon = formData.get("coupon");
+        if (!coupon) {
+            return;
+        }
+        console.log(coupon);
+    };
+
+    return (
+        <form className="flex flex-col gap-2" onSubmit={handleSubmit}>
+            <input type="text" placeholder="Código de descuento" name="coupon" />
             <button type="submit">Aplicar</button>
-        </form> 
-  )
+        </form>
+    );
 }
